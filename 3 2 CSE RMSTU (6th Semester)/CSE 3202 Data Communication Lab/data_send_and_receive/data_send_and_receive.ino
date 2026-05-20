57882f34812eb3b9e1ba6be6ed8384dcdb31983d
Mohammad Kamrul Hassan <stdio.kamrul@gmail.com>
2026-05-20 20:33:47 +0600

    some update commit

---
+++ 3 2 CSE RMSTU (6th Semester)/CSE 3202 Data Communication Lab/data_send_and_receive/data_send_and_receive.ino
@@ -0,0 +1,76 @@
+// Transmitter Code (basic)
+// /*
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS   10
+#define RST  9
+#define DIO0 8
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Team B: LoRa init failed! Check wiring.");
+    while (1);
+  }
+
+  LoRa.setSyncWord(0x12);   // All teams must use same sync word
+  Serial.println("Team B (Arduino) Transmitter Ready");
+}
+
+void loop() {
+  counter++;
+
+  LoRa.beginPacket();
+  LoRa.print("MSG from TeamB | Packet #");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  Serial.print("Team B Sent Packet #");
+  Serial.println(counter);
+
+  delay(3000);
+}
+// */
+
+// Receiver Code (basic)
+/*
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS   10
+#define RST  9
+#define DIO0 8
+
+void setup() {
+  Serial.begin(9600);
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Team B: LoRa init failed! Check wiring.");
+    while (1);
+  }
+
+  LoRa.setSyncWord(0x12);
+  Serial.println("Team B (Arduino) Receiver Ready...");
+  Serial.println("Waiting for packets...");
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+
+  if (packetSize) {
+    Serial.print("Team B Received: ");
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+    Serial.print("  |  RSSI: ");
+    Serial.print(LoRa.packetRssi());
+    Serial.println(" dBm");
+  }
+}
+*/
